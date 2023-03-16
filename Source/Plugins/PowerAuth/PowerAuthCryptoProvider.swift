1b542c5ebb8c7e1d4b1b694f12f417762e631672
Juraj Ďurech <durech.juraj@gmail.com>
2023-03-16 10:37:33 +0100

    Library now uses PowerAuth SDK 1.7.x Min iOS SDK is now 11.0

--- Source/Plugins/PowerAuth/PowerAuthCryptoProvider.swift
+++ Source/Plugins/PowerAuth/PowerAuthCryptoProvider.swift
@@ -14,7 +14,6 @@
 // and limitations under the License.
 //
 
-import PowerAuth2
 import PowerAuthCore
 
 ///
--- Tests/CertStoreTests_NetworkStatic.swift
+++ Tests/CertStoreTests_NetworkStatic.swift
@@ -35,7 +35,7 @@ class CertStoreTests_NetworkStatic: XCTestCase {
      */
     
     // Raw content from my public gist
-    static let serviceUrl = URL(string: "https://gist.githubusercontent.com/hvge/7c5a3f9ac50332a52aa974d90ea2408c/raw/d2ea7150639e1269b9fd54a746e876fa35ed239c/ssl-pinning-signatures.json")!
+    static let serviceUrl = URL(string: "https://gist.githubusercontent.com/hvge/7c5a3f9ac50332a52aa974d90ea2408c/raw/07eb5b4b67e63d37d224912bc5951c7b589b35e6/ssl-pinning-signatures.json")!
     // Public key
     static let publicKey  = "BC3kV9OIDnMuVoCdDR9nEA/JidJLTTDLuSA2TSZsGgODSshfbZg31MS90WC/HdbU/A5WL5GmyDkE/iks6INv+XE="
     
