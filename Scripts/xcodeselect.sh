c48519690007766823dd9f467f791eb7e765edce
Jan Kobersky <5406945+kober32@users.noreply.github.com>
2026-03-31 16:35:48 +0200

    Updated CI scripts and Cartfile (#45)

--- Scripts/xcodeselect.sh
+++ Scripts/xcodeselect.sh
@@ -3,6 +3,17 @@
 set -e
 
 # Script used for selecting proper xcode for all builds on the CI (not appcenter).
-# Available xcodes at https://github.com/actions/runner-images/blob/main/images/macos/macos-15-Readme.md#xcode
+# Available xcodes at https://github.com/actions/runner-images/blob/main/images/macos/macos-26-Readme.md#xcode
 
-sudo xcode-select -s "/Applications/Xcode_16.2.0.app"
+REQUIRED_PATH="/Applications/Xcode_26.3.app"
+CURRENT_PATH=$( xcode-select -p  | sed -E 's/(\.app).*$/\1/' )
+
+echo "Required xcode: ${REQUIRED_PATH}"
+echo "Current xcode:  ${CURRENT_PATH}"
+
+if [[ "${REQUIRED_PATH}" == "${CURRENT_PATH}" ]]; then
+  echo "Required and selected xcode are the same."
+else
+  echo "Selecting ${REQUIRED_PATH}"
+  sudo xcode-select -s ${REQUIRED_PATH}
+fi
