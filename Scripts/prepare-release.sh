6dcd2e9c5be87e7e6afe488a3a7c9bebbedf4705
Marek Chrenko <marek.chrenko@wultra.com>
2026-04-29 10:21:55 +0200

    update prepare-release script

--- Scripts/prepare-release.sh
+++ Scripts/prepare-release.sh
@@ -3,109 +3,42 @@
 set -e # stop script when error occurs
 set -u # stop when undefined variable is used
 set -o pipefail # stop when error occurs in pipe
+#set -x # print all execution (good for debugging)
 
-###############################################################################
-# This script prepares a release with provided version
-# ----------------------------------------------------------------------------
+######### USAGE #########
+#
+# To prepare or verify release metadata of the library (settings proper version, updating changelog, etc.)
+# - this script runs the JavaScript script from Wultra infrastructure repository
+# - it uses data defined in `.prepare-release.json` file in the root of the repository
+# - it passes all parameters to the JavaScript script
+#
+#
+# It can be run in 3 modes:
+#
+# 1. With a version (-v X.Y.Z) argument:
+#  - it will prepare the release with the current version
+#  - use it when you're preparing a new release pull-request
+#  - Example: sh scripts/prepare-release.sh -v 1.0.0
+#
+# 2. With a version argument and --verify:
+#  - it will verify that the given release version is prepared.
+#  - use it to make sure that the release pull-request is properly prepared (also used on CI)
+#  - Example: sh scripts/prepare-release.sh -v 1.0.0 --verify
+#
+# 3. Without arguments:
+#  - it will run the script in the root directory of the repository and verify that all files are prepared.
+#  - use it to make sure that the current state of the repository is ready for release
+#  - Example: sh scripts/prepare-release.sh
+#
+# Note: you can add --ignore-git-clean to ignore "git clean" errors (useful when testing things locally)
+#
+#########################
 
-TOP=$(dirname $0)
-SRC_ROOT="`( cd \"$TOP/..\" && pwd )`"
-DO_COMMIT=0
-DO_PUSH=0
-DO_RELEASE=0
-VERSION=
-VERSIONING_FILES=(
-    "deploy/WultraSSLPinning.podspec,${SRC_ROOT}/WultraSSLPinning.podspec"
-)
+# path to the script folder
+SCRIPT_FOLDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-function USAGE
-{
-    echo ""
-    echo "Usage: prepare-release.sh [options] version"
-    echo ""
-    echo "options are:"
-    echo ""
-    echo "  -c | --commit     commit changed files and create tag"
-    echo ""
-    echo "  -p | --push       push commits and tags"
-    echo ""
-    echo "  -r | --release    release to CocoaPods"
-    echo ""
-    echo "  -h | --help       prints this help information"
-    echo ""
-    exit $1
-}
+# URL of the JavaScript prepare-release script in Wultra infrastructure repository
+URL="https://raw.githubusercontent.com/wultra/wultra-infrastructure/refs/heads/mobile-release/mobile-release/v1/prepare-release.js"
 
-while [[ $# -gt 0 ]]
-do
-    opt="$1"
-    case "$opt" in
-        -c | --commit)
-            DO_COMMIT=1
-            ;;
-        -h | --help)
-            USAGE 0
-            ;;
-        -p | --push)
-            DO_PUSH=1
-            ;;
-        -r | --release)
-            DO_RELEASE=1
-            ;;
-        *)
-            VERSION=$opt
-            ;;
-    esac
-    shift
-done
-
-if [ -z "${VERSION}" ]; then
-    echo "You have to provide version string."
-    exit 1
-fi
-
-echo "Settings version to ${VERSION}."
-
-pushd $TOP
-
-for (( i=0; i<${#VERSIONING_FILES[@]}; i++ ));
-do
-    patch_info="${VERSIONING_FILES[$i]}"
-    files=(${patch_info//,/ })
-    template="${files[0]}"
-    target="${files[1]}"
-    if [ ! -f "$template" ]; then
-        echo "Template file not found: ${template}"
-        exit 1
-    fi
-    if [ ! -f "$target" ]; then
-        echo "Target should exist: ${target}"
-        exit 1
-    fi
-
-    echo "        + ${target}"
-    sed -e "s/%DEPLOY_VERSION%/$VERSION/g" "${template}" > "${target}"
-    if [ x$DO_COMMIT == x1 ]; then
-        git add "${target}"
-    fi
-done
-
-popd
-
-pushd "${SRC_ROOT}"
-
-if [ x$DO_COMMIT == x1 ]; then
-    git commit -m "Bumped version to ${VERSION}"
-    git tag "${VERSION}"
-fi
-
-if [ x$DO_PUSH == x1 ]; then
-    git push --tags
-fi
-
-if [ x$DO_RELEASE == x1 ]; then
-    pod lib lint
-    pod trunk push
-fi
-
-popd
+# execute the remote node and pass all parameters to it + add path parameter to the root of the repository
+curl -fsSL "${URL}" | node - -p "${SCRIPT_FOLDER}/.." "${@}"
