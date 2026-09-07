36a8176c7d5566fc5114eef05b06c1748c38e54b
Jan Kobersky <kober32@gmail.com>
2026-09-07 22:19:34 +0200

    Updated prepare-release.sh + updated readme version

--- Scripts/prepare-release.sh
+++ Scripts/prepare-release.sh
@@ -2,7 +2,6 @@
 
 set -e # stop script when error occurs
 set -u # stop when undefined variable is used
-set -o pipefail # stop when error occurs in pipe
 #set -x # print all execution (good for debugging)
 
 ######### USAGE #########
@@ -18,17 +17,17 @@ set -o pipefail # stop when error occurs in pipe
 # 1. With a version (-v X.Y.Z) argument:
 #  - it will prepare the release with the current version
 #  - use it when you're preparing a new release pull-request
-#  - Example: ./Scripts/prepare-release.sh -v 1.0.0
+#  - Example: sh scripts/prepare-release.sh -v 1.0.0
 #
 # 2. With a version argument and --verify:
 #  - it will verify that the given release version is prepared.
 #  - use it to make sure that the release pull-request is properly prepared (also used on CI)
-#  - Example: ./Scripts/prepare-release.sh -v 1.0.0 --verify
+#  - Example: sh scripts/prepare-release.sh -v 1.0.0 --verify
 #
 # 3. Without arguments:
 #  - it will run the script in the root directory of the repository and verify that all files are prepared.
 #  - use it to make sure that the current state of the repository is ready for release
-#  - Example: ./Scripts/prepare-release.sh
+#  - Example: sh scripts/prepare-release.sh
 #
 # Note: you can add --ignore-git-clean to ignore "git clean" errors (useful when testing things locally)
 #
@@ -38,7 +37,7 @@ set -o pipefail # stop when error occurs in pipe
 SCRIPT_FOLDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # URL of the JavaScript prepare-release script in Wultra infrastructure repository
-URL="https://raw.githubusercontent.com/wultra/wultra-infrastructure/refs/heads/mobile-release/mobile-release/v1/prepare-release.js"
+URL="https://raw.githubusercontent.com/wultra/wultra-infrastructure/refs/heads/mobile/mobile/release/prepare/prepare-release.js"
 
 # execute the remote node and pass all parameters to it + add path parameter to the root of the repository
 curl -fsSL "${URL}" | node - -p "${SCRIPT_FOLDER}/.." "${@}"
